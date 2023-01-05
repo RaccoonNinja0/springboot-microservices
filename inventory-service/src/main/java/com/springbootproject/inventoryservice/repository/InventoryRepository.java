e23ba4b7003cbd6715337672bfc46233e0f5b13d
RaccoonNinja0 <97842065+RaccoonNinja0@users.noreply.github.com>
2023-01-04 20:55:51 -0500

    Added inter-communication for services (#1)

--- inventory-service/src/main/java/com/springbootproject/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/springbootproject/inventoryservice/repository/InventoryRepository.java
@@ -1,11 +1,14 @@
 package com.springbootproject.inventoryservice.repository;
 
+import com.fasterxml.jackson.databind.introspect.AnnotationCollector;
 import com.springbootproject.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-    Optional<Inventory> findBySkuCode(String skuCode);
+
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
 }
--- order-service/src/main/java/com/springbootproject/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/springbootproject/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.springbootproject.orderservice.service;
 
+import com.springbootproject.orderservice.dto.InventoryResponse;
 import com.springbootproject.orderservice.dto.OrderLineItemsDto;
 import com.springbootproject.orderservice.dto.OrderRequest;
 import com.springbootproject.orderservice.model.Order;
@@ -7,7 +8,9 @@ import com.springbootproject.orderservice.model.OrderLineItems;
 import com.springbootproject.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,7 +19,8 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    public void placeOrder(OrderRequest orderRequest){
+    private final WebClient webClient;
+    public void placeOrder(OrderRequest orderRequest) throws IllegalAccessException {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
         List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
@@ -24,7 +28,27 @@ public class OrderService {
                 .map(orderLineItemsDto -> mapToDto(orderLineItemsDto))
                 .toList();
         order.setOrderLineItemsList(orderLineItems);
-        orderRepository.save(order);
+
+        List<String> skuCodes = order.getOrderLineItemsList().stream().map(orderLineItem ->
+                        orderLineItem
+                                .getSkuCode()
+                                .toString())
+                                .toList();
+        //communicate with inventory service using webClient to check product availability
+        //append skucodes using uriBuilder.queryParam
+        //the query will return InventoryResponse array in which every response has the isInStock boolean for each item.
+        InventoryResponse[] stockResults = webClient.get().uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCodes", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean allProductInStock = Arrays.stream(stockResults).allMatch(inventoryResponse -> inventoryResponse.isInStock());
+        if(allProductInStock){
+            orderRepository.save(order);
+        } else {
+            throw new IllegalAccessException("Product is out of stock");
+        }
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
