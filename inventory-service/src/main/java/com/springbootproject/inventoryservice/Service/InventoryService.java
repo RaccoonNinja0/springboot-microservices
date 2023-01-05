cf4c9378dff808ee85f5f8e86bae2fa05e17bff6
chenhaowei <chenhaowei199901@outlook.com>
2023-01-04 20:39:08 -0500

    Added inter-communication for services

--- inventory-service/src/main/java/com/springbootproject/inventoryservice/Service/InventoryService.java
+++ inventory-service/src/main/java/com/springbootproject/inventoryservice/Service/InventoryService.java
@@ -1,18 +1,27 @@
-package com.springbootproject.inventoryservice.Service;
+package com.springbootproject.inventoryservice.service;
 
+import com.springbootproject.inventoryservice.dto.InventoryResponse;
 import com.springbootproject.inventoryservice.repository.InventoryRepository;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
     private final InventoryRepository inventoryRepository;
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode){
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode){
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                    InventoryResponse.builder()
+                            .skuCode(inventory.getSkuCode())
+                            .isInStock(inventory.getQuantity() > 0)
+                            .build()
+                ).toList();
     }
 }
--- inventory-service/src/main/java/com/springbootproject/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/springbootproject/inventoryservice/controller/InventoryController.java
@@ -1,9 +1,12 @@
 package com.springbootproject.inventoryservice.controller;
 
-import com.springbootproject.inventoryservice.Service.InventoryService;
+import com.springbootproject.inventoryservice.dto.InventoryResponse;
+import com.springbootproject.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/api/inventory")
 @RestController
@@ -11,9 +14,9 @@ import org.springframework.web.bind.annotation.*;
 public class InventoryController {
     private final InventoryService inventoryService;
 
-    @GetMapping("/{sku-code}")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode){
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode){
         return inventoryService.isInStock(skuCode);
     }
 }
