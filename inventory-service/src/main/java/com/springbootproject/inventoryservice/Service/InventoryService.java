97ec7d365e5739410702bea66e2d1109f7302ee5
chenhaowei <chenhaowei199901@outlook.com>
2023-01-04 20:40:31 -0500

    Revert "Added inte-communication of services"
    This reverts commit f51549072a18d63f3c020949e1b081d2d69d2f1b.

--- inventory-service/src/main/java/com/springbootproject/inventoryservice/Service/InventoryService.java
+++ inventory-service/src/main/java/com/springbootproject/inventoryservice/Service/InventoryService.java
@@ -1,27 +1,18 @@
-package com.springbootproject.inventoryservice.service;
+package com.springbootproject.inventoryservice.Service;
 
-import com.springbootproject.inventoryservice.dto.InventoryResponse;
 import com.springbootproject.inventoryservice.repository.InventoryRepository;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class InventoryService {
     private final InventoryRepository inventoryRepository;
     @Transactional(readOnly = true)
-    public List<InventoryResponse> isInStock(List<String> skuCode){
-        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
-                .map(inventory ->
-                    InventoryResponse.builder()
-                            .skuCode(inventory.getSkuCode())
-                            .isInStock(inventory.getQuantity() > 0)
-                            .build()
-                ).toList();
+    public boolean isInStock(String skuCode){
+        return inventoryRepository.findBySkuCode(skuCode).isPresent();
     }
 }
--- order-service/src/main/java/com/springbootproject/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/springbootproject/orderservice/service/OrderService.java
@@ -1,6 +1,5 @@
 package com.springbootproject.orderservice.service;
 
-import com.springbootproject.orderservice.dto.InventoryResponse;
 import com.springbootproject.orderservice.dto.OrderLineItemsDto;
 import com.springbootproject.orderservice.dto.OrderRequest;
 import com.springbootproject.orderservice.model.Order;
@@ -8,9 +7,7 @@ import com.springbootproject.orderservice.model.OrderLineItems;
 import com.springbootproject.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -19,8 +16,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
-    public void placeOrder(OrderRequest orderRequest) throws IllegalAccessException {
+    public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
         List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
@@ -28,27 +24,7 @@ public class OrderService {
                 .map(orderLineItemsDto -> mapToDto(orderLineItemsDto))
                 .toList();
         order.setOrderLineItemsList(orderLineItems);
-
-        List<String> skuCodes = order.getOrderLineItemsList().stream().map(orderLineItem ->
-                        orderLineItem
-                                .getSkuCode()
-                                .toString())
-                                .toList();
-        //communicate with inventory service using webClient to check product availability
-        //append skucodes using uriBuilder.queryParam
-        //the query will return InventoryResponse array in which every response has the isInStock boolean for each item.
-        InventoryResponse[] stockResults = webClient.get().uri("http://localhost:8082/api/inventory",
-                        uriBuilder -> uriBuilder.queryParam("skuCodes", skuCodes).build())
-                .retrieve()
-                .bodyToMono(InventoryResponse[].class)
-                .block();
-
-        boolean allProductInStock = Arrays.stream(stockResults).allMatch(inventoryResponse -> inventoryResponse.isInStock());
-        if(allProductInStock){
-            orderRepository.save(order);
-        } else {
-            throw new IllegalAccessException("Product is out of stock");
-        }
+        orderRepository.save(order);
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
